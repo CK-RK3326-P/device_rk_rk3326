b6e9c81544e6bde15f7224ac17688348aaf82288
milab <milab@qq.com>
2020-03-12 11:29:38 +0800

    rotate 270

--- h710/BoardConfig.mk
+++ h710/BoardConfig.mk
@@ -23,6 +23,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
+SF_PRIMARY_DISPLAY_ORIENTATION := 270
 
 # Config GO Optimization
 BUILD_WITH_GO_OPT := true
